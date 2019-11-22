12cc0e9d579cc32776308822354f5d1728318838
Paul Grau <paul@blocko.io>
2019-11-22 12:28:53 +0900

    remove some console.log

--- src/content-script.js
+++ src/content-script.js
@@ -7,7 +7,7 @@ window.addEventListener("message", (event) => {
         return;
 
     if (event.data.type && (event.data.type == "AERGO_REQUEST")) {
-        console.log("[Contentscript] Received message from website", event.data);
+        // console.log("[Contentscript] Received message from website", event.data);
         port.postMessage(event.data);
     }
 });
@@ -15,7 +15,7 @@ window.addEventListener("message", (event) => {
 // Message from extension to website
 port.onMessage.addListener((msg) => {
     if (msg.type !== 'AERGO_RESPONSE' && msg.type !== 'AERGO_CANCEL') return;
-    console.log('[Contentscript] Received message from extension', msg);
+    // console.log('[Contentscript] Received message from extension', msg);
     let event = new CustomEvent(msg.eventName, { detail: msg.result });
     window.dispatchEvent(event);
 });
